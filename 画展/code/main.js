6bacf4606e9089d663703b3492293f3e40c6f040
gusongen <1572055146@qq.com>
2019-03-09 13:52:34 +0800

    modify

---
+++ 画展/code/main.js
@@ -0,0 +1,11 @@
+
+
+var i=0;
+function displayDate()
+{ //document.getElementsByClassName("button").style.color="red";
+    i++;
+  if(i==3)
+  alert("没用的别点了");
+if(i==10)
+window.open('./彩蛋.html','_blank');
+}
